dbde7bddf5b44d1b17132d0a050321d79d3e8a48
Marine Kelley <marinekelley@gmail.com>
2011-09-03 08:59:37 +0200

    Change the default "blocked send IM message" back to "IM blocked by sender's viewer", as "..." was confusing a lot of people

--- indra/newview/RRInterface.cpp
+++ indra/newview/RRInterface.cpp
@@ -79,7 +79,7 @@ BOOL RRInterface::sRRNoSetEnv = FALSE;
 BOOL RRInterface::sRestrainedLoveDebug = FALSE;
 BOOL RRInterface::sCanOoc = TRUE;
 std::string RRInterface::sRecvimMessage = "The Resident you messaged is prevented from reading your instant messages at the moment, please try again later.";
-std::string RRInterface::sSendimMessage = "...";
+std::string RRInterface::sSendimMessage = "*** IM blocked by sender's viewer";
 
 
 #if !defined(max)
