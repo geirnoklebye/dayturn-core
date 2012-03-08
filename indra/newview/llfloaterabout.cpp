4456a2029875d07bb71e162986817aa01bed1335
Nicky Perian <nickyperian@yahoo.com>
2012-03-07 22:37:17 -0600

    By-pass server release notes search. Causes much warning spam on opensim grids. Needs looked at after grid manager is in place.

--- indra/newview/llfloaterabout.cpp
+++ indra/newview/llfloaterabout.cpp
@@ -142,7 +142,7 @@ BOOL LLFloaterAbout::postBuild()
 
 	getChild<LLUICtrl>("copy_btn")->setCommitCallback(
 		boost::bind(&LLFloaterAbout::onClickCopyToClipboard, this));
-
+/* FIXME NP opemsim floos of warnings.
 	if (gAgent.getRegion())
 	{
 		// start fetching server release notes URL
@@ -153,6 +153,8 @@ BOOL LLFloaterAbout::postBuild()
 	{
 		setSupportText(LLStringUtil::null);
 	}
+*/
+	setSupportText(LLStringUtil::null);
 
 	support_widget->blockUndo();
 
