dec970eed5e707d95bfcb28e800920b8950226c3
Nicky Perian <nickyperian@yahoo.com>
2012-06-18 18:56:29 -0500

    hack in project test to prevent build failure related to non use of apr lib 1.45. Falling back to apr 1.39. apr 1.45 has runtime issues

--- indra/test/namedtempfile.h
+++ indra/test/namedtempfile.h
@@ -196,7 +196,11 @@ private:
         // extension) is also unique. This is likely, though not guaranteed:
         // files could be created in the same temp directory other than by
         // this class.
-        ll_apr_assert_status(apr_file_link(mPath.c_str(), path.c_str()));
+        //*HACK building with 2.6.32-5-amd64 x86_64 GNU/Linux
+        // g++ (Debian 4.4.5-8) 4.4.5
+        // Commented out because apr_file_link not declared in this scope and
+        // Cannot find anything by that name on this system
+        //ll_apr_assert_status(apr_file_link(mPath.c_str(), path.c_str()));
     }
 
     std::string mLink;
