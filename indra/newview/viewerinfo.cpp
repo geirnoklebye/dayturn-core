5be3f297bbcad4a41432ca79d920bc08cb43e579
Nicky Perian <nickyperian@yahoo.com>
2012-06-19 20:42:40 -0500

    Version to Kokua-3.3.3.23042-Beta_1c

--- indra/newview/viewerinfo.cpp
+++ indra/newview/viewerinfo.cpp
@@ -34,8 +34,8 @@ namespace ViewerInfo
 	const S32         MAJOR = 3;
 	const S32         MINOR = 3;
 	const S32         PATCH = 3;
-	const S32         BUILD = 29300;
-	const std::string EXTRA = "Beta_1b";
+	const S32         BUILD = 23402;
+	const std::string EXTRA = "Beta_1c";
 
 
 	const std::string& viewerName()
