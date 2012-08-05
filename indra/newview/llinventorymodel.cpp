1971053787ec60bdc36b3271252539348adbce09
Marine Kelley <marinekelley@gmail.com>
2012-08-05 17:26:58 +0200

    Small fixes to prevent the user from moving an item or a folder to/from a locked folder

--- indra/newview/llinventorymodel.cpp
+++ indra/newview/llinventorymodel.cpp
@@ -1047,7 +1047,7 @@ void LLInventoryModel::changeItemParent(LLViewerInventoryItem* item,
 			LLInventoryCategory* cat_parent = gInventory.getCategory (item->getParentUUID());
 			LLInventoryCategory* cat_new_parent = gInventory.getCategory (new_parent_id);
 			// We can move this item if we are moving it from an unshared folder to another one, even if both folders are locked
-			if (gAgent.mRRInterface.isUnderRlvShare(cat_parent) || gAgent.mRRInterface.isUnderRlvShare(cat_new_parent))
+//			if (gAgent.mRRInterface.isUnderRlvShare(cat_parent) || gAgent.mRRInterface.isUnderRlvShare(cat_new_parent))
 			{
 				if (gAgent.mRRInterface.isFolderLocked(cat_parent) || gAgent.mRRInterface.isFolderLocked(cat_new_parent))
 				{
@@ -1089,7 +1089,7 @@ void LLInventoryModel::changeCategoryParent(LLViewerInventoryCategory* cat,
 	{
 		LLInventoryCategory* cat_new_parent = gInventory.getCategory (new_parent_id);
 			// We can move this category if we are moving it from an unshared folder to another one, even if both folders are locked
-			if (gAgent.mRRInterface.isUnderRlvShare(cat) || gAgent.mRRInterface.isUnderRlvShare(cat_new_parent))
+//			if (gAgent.mRRInterface.isUnderRlvShare(cat) || gAgent.mRRInterface.isUnderRlvShare(cat_new_parent))
 		{
 			if (gAgent.mRRInterface.isFolderLocked(cat) || gAgent.mRRInterface.isFolderLocked(cat_new_parent))
 			{
