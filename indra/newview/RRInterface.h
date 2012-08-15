fe2f486b14ee49116c0ff298a730cb7ea8bee0cc
Marine Kelley <marinekelley@gmail.com>
2012-08-15 23:11:44 +0200

    Reinstatement of the HACD library after including the Pathfinding stuff.

--- indra/newview/RRInterface.h
+++ indra/newview/RRInterface.h
@@ -28,7 +28,7 @@
 #define RR_VIEWER_NAME_NEW "RestrainedLove"
 #define RR_VERSION_NUM "2080304"
 #define RR_VERSION "2.08.03.04"
-#define RR_SLV_VERSION "3.3.4.24798"
+#define RR_SLV_VERSION "3.3.4.24842"
 
 #define RR_PREFIX "@"
 #define RR_SHARED_FOLDER "#RLV"
