bcc6ba6ed5eb8966ab4f75cfb08791bf7bff18ca
Nicky Perian <nickyperian@yahoo.com>
2012-11-05 14:11:46 -0600

    Version to 3.4.1/24900

--- indra/newview/viewerinfo.cpp
+++ indra/newview/viewerinfo.cpp
@@ -34,7 +34,7 @@ namespace ViewerInfo
 	const S32         MAJOR = 3;
 	const S32         MINOR = 4;
 	const S32         PATCH = 1;
-	const S32         BUILD = 24848;
+	const S32         BUILD = 24900;
 	const std::string EXTRA = "";
 
 
