dc3c6ea87aa78323b66f7f0c40276af2ce417e7a
LanceCorrimal <none@none>
2012-11-17 16:57:21 +0100

    Getting rid of last traces of old HACD

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -6,6 +6,5 @@ if (NOT STANDALONE)
   use_prebuilt_binary(libuuid)
   use_prebuilt_binary(slvoice)
   use_prebuilt_binary(fontconfig)
-  use_prebuilt_binary(k_hacdConvexDecomposition)
 endif(NOT STANDALONE)
 
