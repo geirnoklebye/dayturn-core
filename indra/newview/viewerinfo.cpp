e28a012121995d69b7898df447cfb3340ddee1fc
Nicky Perian <nickyperian@yahoo.com>
2012-12-04 19:13:27 -0600

    Set version changeset

--- indra/newview/viewerinfo.cpp
+++ indra/newview/viewerinfo.cpp
@@ -34,7 +34,7 @@ namespace ViewerInfo
 	const S32         MAJOR = 3;
 	const S32         MINOR = 4;
 	const S32         PATCH = 2;
-	const S32         BUILD = 25200;
+	const S32         BUILD = 25211;
 	const std::string EXTRA = "";
 
 
