5b91caeb9487e7d8946b96e000ad23124302eece
Nicky Perian <nickyperian@yahoo.com>
2013-01-17 14:38:36 -0600

    Set Version Kokua-3.4.4.25641

--- indra/newview/viewerinfo.cpp
+++ indra/newview/viewerinfo.cpp
@@ -34,7 +34,7 @@ namespace ViewerInfo
 	const S32         MAJOR = 3;
 	const S32         MINOR = 4;
 	const S32         PATCH = 4;
-	const S32         BUILD = 25633;
+	const S32         BUILD = 25641;
 	const std::string EXTRA = "";
 
 
