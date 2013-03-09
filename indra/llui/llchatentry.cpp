c5170ff11ec2e0fce046ad7eecef28cb7223ec9e
Marine Kelley <marinekelley@gmail.com>
2013-03-09 10:11:04 +0100

    Do not erase a selection when pressing RETURN on the chat bar, because it makes gestures much more difficult to trigger otherwise.

--- indra/llui/llchatentry.cpp
+++ indra/llui/llchatentry.cpp
@@ -169,6 +169,17 @@ BOOL LLChatEntry::handleSpecialKey(const KEY key, const MASK mask)
 {
 	BOOL handled = FALSE;
 
+//MK
+	// In the case of a chat entry, pressing RETURN when something is selected
+	// should NOT erase the selection (unlike a notecard, for example)
+	if (key == KEY_RETURN)
+	{
+		endOfDoc();
+		startSelection();
+		endSelection();
+	}
+//mk
+
 	LLTextEditor::handleSpecialKey(key, mask);
 
 	switch(key)
