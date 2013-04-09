088c518d14efb01a5526c90d9ba7af7b63060311
Cinders <cinder@cinderblocks.biz>
2013-04-08 18:09:21 -0600

    FIRE-9682: Disable import/export via setting (default: off) - This can be backed out once exports are stable!

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -8416,7 +8416,11 @@ void show_topinfobar_context_menu(LLView* ctrl, S32 x, S32 y)
 // <FS:Techwolf Lupindo> export
 BOOL enable_export_object()
 {
-	return LLSelectMgr::getInstance()->selectGetAllValid();
+    // <FS:CR> FIRE-9682 - Temporarily disable export by setting (default off)
+	//return LLSelectMgr::getInstance()->selectGetAllValid();
+    bool allow_export = (LLSelectMgr::getInstance()->selectGetAllValid() && gSavedSettings.getBOOL("FSEnableObjectExports"));
+    return allow_export;
+    // </FS:CR>
 }
 
 class FSObjectExport : public view_listener_t
