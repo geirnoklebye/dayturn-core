308d18cbd3215afdcb7853f5ff213e3fc1ead208
Nicky Perian <nickyperian@yahoo.com>
2013-04-13 10:37:10 -0500

    Set Version kokua-3.5.2.27963

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -30,7 +30,7 @@
 const S32 LL_VERSION_MAJOR = 3;
 const S32 LL_VERSION_MINOR = 5;
 const S32 LL_VERSION_PATCH = 2;
-const S32 LL_VERSION_BUILD = 27437;
+const S32 LL_VERSION_BUILD = 27963;
 
 const char * const LL_CHANNEL = "Kokua Beta";
 // For example, "Kokua Experimental" will create an installer string of Kokua_Experimental_[VERSION]_Setup.exe
