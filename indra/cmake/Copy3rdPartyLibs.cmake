ae39fd28dbddb7226730d41184b625c9f145af79
Nicky Perian <nickyperian@yahoo.com>
2013-05-06 21:00:35 -0500

    {Linux 64 bit]Remove llbbreakpad_client.so as a 3rd party library,

--- indra/cmake/Copy3rdPartyLibs.cmake
+++ indra/cmake/Copy3rdPartyLibs.cmake
@@ -404,7 +404,7 @@ elseif(LINUX)
           libboost_filesystem-mt.so.${BOOST_VERSION}.0
           libboost_signals-mt.so.${BOOST_VERSION}.0
           libboost_system-mt.so.${BOOST_VERSION}.0
-          libbreakpad_client.so.0
+#          libbreakpad_client.so.0
           libcares.so.2
           libcrypto.so
           libcrypto.so.1.0.0
