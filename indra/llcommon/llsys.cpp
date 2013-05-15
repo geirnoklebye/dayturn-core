4cde768c9308184ce3c76955466441a28c02aa16
Tank_Master <tank.master@phoenixviewer.com>
2013-05-15 14:50:48 -0700

    State Windows 8.1 instead of its codname of Windows Blue for NT 6.3

--- indra/llcommon/llsys.cpp
+++ indra/llcommon/llsys.cpp
@@ -283,6 +283,11 @@ LLOSInfo::LLOSInfo() :
 					else
 						mOSStringSimple = "Windows Server 2012 ";
 				}
+				else if(osvi.dwMinorVersion == 3)
+				{
+					if(osvi.wProductType == VER_NT_WORKSTATION)
+						mOSStringSimple = "Microsoft Windows 8.1 ";
+				}
 
 				///get native system info if available..
 				typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO); ///function pointer for loading GetNativeSystemInfo
