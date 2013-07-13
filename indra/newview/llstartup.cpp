6570c50d84cf20065b49b1b66c283965713536aa
Jessica Wabbit <jessica.wabbit@outlook.com>
2013-07-13 04:16:29 +0100

    Set the maximum number of groups correctly on OpenSim grids.

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -3580,10 +3580,16 @@ bool process_login_success_response()
 		LLViewerMedia::openIDSetup(openid_url, openid_token);
 	}
 
-	if(response.has("max-agent-groups")) {		
+	if (response.has("max_groups")) {		
+		std::string max_agent_groups(response["max_groups"]);
+		gMaxAgentGroups = atoi(max_agent_groups.c_str());
+		LL_INFOS("LLStartup") << "gMaxAgentGroups (max_groups) read from login.cgi: "
+							  << gMaxAgentGroups << LL_ENDL;
+	}
+	else if (response.has("max-agent-groups")) {		
 		std::string max_agent_groups(response["max-agent-groups"]);
 		gMaxAgentGroups = atoi(max_agent_groups.c_str());
-		LL_INFOS("LLStartup") << "gMaxAgentGroups read from login.cgi: "
+		LL_INFOS("LLStartup") << "gMaxAgentGroups (max-agent-groups) read from login.cgi: "
 							  << gMaxAgentGroups << LL_ENDL;
 	}
 	else {
