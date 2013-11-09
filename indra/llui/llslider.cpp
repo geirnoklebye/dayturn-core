d9a4e7449bce0ac18db6ecec88baa513ab221797
Jessica Wabbit <jessica.wabbit@outlook.com>
2013-11-09 19:12:24 +0000

    Slider widgets can now be adjusted with the mouse's scroll wheel.

--- indra/llui/llslider.cpp
+++ indra/llui/llslider.cpp
@@ -281,13 +281,9 @@ BOOL LLSlider::handleKeyHere(KEY key, MASK mask)
 
 BOOL LLSlider::handleScrollWheel(S32 x, S32 y, S32 clicks)
 {
-	if ( mOrientation == VERTICAL )
-	{
-		F32 new_val = getValueF32() - clicks * getIncrement();
-		setValueAndCommit(new_val);
-		return TRUE;
-	}
-	return LLF32UICtrl::handleScrollWheel(x,y,clicks);
+	setValueAndCommit(getValueF32() - clicks * getIncrement());
+
+	return TRUE;
 }
 
 void LLSlider::draw()
