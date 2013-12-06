142df421ff672e5f0efc1a2f55c5083a4358e064
Monty Brandenberg <monty@lindenlab.com>
2013-12-06 16:02:53 -0500

    SH-4645 Viewer hangs on exit after cancelling a mesh upload. Problem involved a 3-way livelock between the main, upload and decomposition threads.  Viewer is shutting down but an upload is in the 'generate hulls' state.  Main thread asks upload request to discard and spins waiting for it to finish. Upload thread is in generateHulls spinning waiting for the decomposition thread to process a mesh request.  Decomposition thread is sleeping waiting for main thread to deliver work that upload thread has asked the decomposition thread to do.

--- indra/newview/llmeshrepository.h
+++ indra/newview/llmeshrepository.h
@@ -519,7 +519,7 @@ public:
 	bool			mUploadTextures;
 	bool			mUploadSkin;
 	bool			mUploadJoints;
-	BOOL            mDiscarded;
+	volatile bool	mDiscarded;
 
 	LLHost			mHost;
 	std::string		mWholeModelFeeCapability;
@@ -535,7 +535,7 @@ public:
 	virtual void run();
 	void preStart();
 	void discard() ;
-	BOOL isDiscarded() const;
+	bool isDiscarded() const;
 
 	void generateHulls();
 
