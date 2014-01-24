60e31fde60b15dc17aded80d8d44d84f649fb46e
Nicky Perian <nickyperian@yahoo.com>
2014-01-23 19:00:28 -0600

    [Darwin] add --no-configure and-- -j 3 change storage directory to Nightly

--- scripts/automated_build_scripts/build-kokua-nightly-darwin32.sh
+++ scripts/automated_build_scripts/build-kokua-nightly-darwin32.sh
@@ -28,7 +28,7 @@ date  2>&1 |tee Darwin.log
 hg pull -u  2>&1 |tee -a Darwin.log
 ~/autobuild/bin/autobuild configure -v -c ReleaseOS -- $defs_extra -DLL_TESTS=OFF -DFMODEX=OFF -DOPENAL=ON -DPACKAGE=ON -DQUICKTIME=ON -DVIEWER_CHANNEL="\"Kokua Test\""
 #check for errors
-~/autobuild/bin/autobuild build -c ReleaseOS 2>&1 |tee -a Darwin.log
+~/autobuild/bin/autobuild build -c ReleaseOS --no-configure -- -j 3 2>&1 |tee -a Darwin.log
 date 2>&1 |tee -a Darwin.log
 #check for errors
 cp -v Darwin*.log ~/NightBuildResult/ 2>&1 |tee -a Darwin.log
@@ -36,7 +36,7 @@ popd
 pushd "$BUILD_DIR/newview"
 cp -v *.dmg ~/NightBuildResult/ 2>&1 |tee -a Darwin.log
 popd
-rsync -avP -e ssh ~/NightBuildResult/*.dmg ~/NightBuildResult/Darwin.log nickyp@frs.sourceforge.net:/home/frs/project/kokua.team-purple.p/UploadTest/
+rsync -avP -e ssh ~/NightBuildResult/*.dmg ~/NightBuildResult/Darwin.log nickyp@frs.sourceforge.net:/home/frs/project/kokua.team-purple.p/Nightly/
 #sudo shutdown -h now
 
 
