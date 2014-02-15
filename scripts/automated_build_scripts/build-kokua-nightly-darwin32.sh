ccf663fb63644dca9313a5df952d2400a3ba7955
Nicky Perian <nickyperian@yahoo.com>
2014-02-16 03:16:10 +1000

    [Darwin] Make the build process simpler and work on OSX 10.6.8 machines Task #1330

--- scripts/automated_build_scripts/build-kokua-nightly-darwin32.sh
+++ scripts/automated_build_scripts/build-kokua-nightly-darwin32.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 #export AUTOBUILD_PLATFORM_OVERRIDE='linux64'
 TOP="/Users/nicky"
-## Following adapted from onefang Rejected's Imprudence build sctipts
-# Try to figure out which SDK is the earliest this version of Xcode
-# supports.  This might be fragile, only tested it with Xcode 3.2.6,
-# which is the minimum supported Xcode anyway.
-sdk_path="$(xcodebuild -version -sdk | grep Path: | cut -d ' ' -f 2 | \
-grep MacOSX10. | sort | head -n 1)"
-# Save both parts, in case there's stuff in the path that the magic
-# foo below might get confused over.
-sdk_file="$(basename $sdk_path)"
-sdk_path="$(dirname $sdk_path)"
-sdk="$(echo $sdk_file | cut -d '.' -f 2)"
-
-# Minimum of SDK 10.5
-if [ "$sdk" == "4u" ] ; then sdk_file="MacOSX10.5.sdk"; fi
-
-defs_extra="-DCMAKE_OSX_SYSROOT=$sdk_path/$sdk_file"
 ##
 #rm -Rf "$TOP/kokua/build-darwin-i386"
 SOURCE_DIR="$TOP/kokua/indra/newview"
@@ -26,7 +10,7 @@ pushd "$TOP/kokua"
 rm -v -f ~/NightBuildResult/Darwin.log
 date  2>&1 |tee Darwin.log
 hg pull -u  2>&1 |tee -a Darwin.log
-~/autobuild/bin/autobuild configure -v -c ReleaseOS -- $defs_extra -DLL_TESTS=OFF -DFMODEX=OFF -DOPENAL=ON -DPACKAGE=ON -DQUICKTIME=ON -DVIEWER_CHANNEL="\"Kokua Test\""
+~/autobuild/bin/autobuild configure -v -c ReleaseOS -- -DLL_TESTS=OFF -DFMODEX=OFF -DOPENAL=ON -DPACKAGE=ON -DQUICKTIME=ON -DVIEWER_CHANNEL="\"Kokua Test\""
 #check for errors
 ~/autobuild/bin/autobuild build -c ReleaseOS --no-configure -- -j 3 2>&1 |tee -a Darwin.log
 date 2>&1 |tee -a Darwin.log
