6ef2af266b3da5c0b91477252b220d857a848e75
Nicky Perian <nickyperian@yahoo.com>
2014-04-21 13:57:01 -0500

    [Linux] merge error getGridID vs getGridLabsl

--- indra/newview/llappviewerlinux.cpp
+++ indra/newview/llappviewerlinux.cpp
@@ -352,7 +352,7 @@ void LLAppViewerLinux::initCrashReporting(bool reportFreeze)
 	const char * cmdargv[] =
 		{cmd.c_str(),
 		 "-user",
-		 (char*)LLGridManager::getInstance()->getGridId().c_str(),
+			 (char*)LLGridManager::getInstance()->getGridLabel().c_str(),
 		 "-name",
 		 LLAppViewer::instance()->getSecondLifeTitle().c_str(),
 		 "-pid", 
