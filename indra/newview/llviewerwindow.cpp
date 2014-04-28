bd71bbc1d57a2e76653fc6a86d9f375b2d77f7ae
Nicky Perian <nickyperian@yahoo.com>
2014-04-28 16:49:54 -0500

    [Linux]Backed out changeset: 365375529cd2 Mouse sensitivity low, cannot be adjusted. https://jira.secondlife.com/browse/BUG-5380

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -3332,6 +3332,8 @@ void LLViewerWindow::updateUI()
 
 	updateLayout();
 
+	mLastMousePoint = mCurrentMousePoint;
+
 	// cleanup unused selections when no modal dialogs are open
 	if (LLModalDialog::activeCount() == 0)
 	{
@@ -3569,9 +3571,6 @@ void LLViewerWindow::saveLastMouse(const LLCoordGL &point)
 {
 	// Store last mouse location.
 	// If mouse leaves window, pretend last point was on edge of window
-
-	mLastMousePoint = mCurrentMousePoint;
-
 	if (point.mX < 0)
 	{
 		mCurrentMousePoint.mX = 0;
--- indra/newview/llworldmapview.cpp
+++ indra/newview/llworldmapview.cpp
@@ -1599,6 +1599,14 @@ void LLWorldMapView::handleClick(S32 x, S32 y, MASK mask,
 }
 
 
+BOOL outside_slop(S32 x, S32 y, S32 start_x, S32 start_y)
+{
+	S32 dx = x - start_x;
+	S32 dy = y - start_y;
+
+	return (dx <= -2 || 2 <= dx || dy <= -2 || 2 <= dy);
+}
+
 BOOL LLWorldMapView::handleMouseDown( S32 x, S32 y, MASK mask )
 {
 	gFocusMgr.setMouseCapture( this );
@@ -1681,7 +1689,7 @@ BOOL LLWorldMapView::handleHover( S32 x, S32 y, MASK mask )
 {
 	if (hasMouseCapture())
 	{
-		if (mPanning || llabs(x - mMouseDownX) > 1 || llabs(y - mMouseDownY) > 1)
+		if (mPanning || outside_slop(x, y, mMouseDownX, mMouseDownY))
 		{
 			// just started panning, so hide cursor
 			if (!mPanning)
