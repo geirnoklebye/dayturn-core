8520360a11e7bd2301813459880aef8f7f811cea
Jessica Wabbit <jessica.wabbit@outlook.com>
2014-05-04 05:50:08 +0100

    Pre-load the Preferences floater as it can be slow to open the first time.

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -1956,6 +1956,9 @@ void LLViewerWindow::initWorldUI()
 	LLFloaterReg::getInstance("build");
 	LLFloaterReg::hideInstance("build");
 
+	// Pre-load the Preferenes floater
+	LLFloaterReg::getInstance("preferences");
+
 	// Status bar
 	LLPanel* status_bar_container = getRootView()->getChild<LLPanel>("status_bar_container");
 	gStatusBar = new LLStatusBar(status_bar_container->getLocalRect());
