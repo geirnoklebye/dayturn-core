bff0d67c0832861b02243d1037b9cb109264498e
Nicky Perian <nickyperian@yahoo.com>
2014-05-17 19:52:21 -0500

    Bug #1381 Viewer interesting merge - llviewertexture.cpp 1242--FS code is commented out Remove and use viewer-release / interesting method to manage texture memory Original FS Changeset: (2caef9001b37) Introduce FSDestroyGLTexturesThreshold setting to make threshold for dumping GL textures off memory configurable

--- indra/newview/llviewertexture.cpp
+++ indra/newview/llviewertexture.cpp
@@ -1238,16 +1238,6 @@ void LLViewerFetchedTexture::destroyTexture()
 	{
 		return ;
 	}
-	//               we reach the desired max texture memory!
-/*
-	static LLCachedControl<bool> fsDestroyGLTexturesImmediately(gSavedSettings, "FSDestroyGLTexturesImmediately", false);
-	static LLCachedControl<F32> fsDestroyGLTexturesThreshold(gSavedSettings, "FSDestroyGLTexturesThreshold", 0.9f);
-	if (!fsDestroyGLTexturesImmediately && LLImageGL::sGlobalTextureMemory < sMaxDesiredTextureMem * fsDestroyGLTexturesThreshold)//not ready to release unused memory.
-	{
-		return ;
-	}
-	// </FS:Ansariel>
-*/
 	if (mNeedsCreateTexture)//return if in the process of generating a new texture.
 	{
 		return;
