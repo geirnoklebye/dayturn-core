f3a3a516d396278fb1dbe8ce1cd08aadf770a82d
Nicky Perian <nickyperian@yahoo.com>
2014-06-20 17:10:02 -0500

    Changed per Cinder Roxley review so that the case of opensim using server bakes is covered.

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -4862,7 +4862,7 @@ const std::string LLVOAvatar::getImageURL(const U8 te, const LLUUID &uuid)
 		if (appearance_service_url.empty())
 		{
 			// Probably a server-side issue if we get here:
-     		if (gSimulatorType == "SecondLife") //OpemSim does not have Server Side Baking
+     		if (gAgent.getRegion()->getCentralBakeVersion() ) //Doubly check for Server Bakes
 	    	{
 	    		LL_WARNS() << "AgentAppearanceServiceURL not set - Baked texture requests will fail" << LL_ENDL;
 		    }
