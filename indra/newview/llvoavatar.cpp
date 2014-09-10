97ee1ca4dd0df2383438dec5f2ce6e662376121b
Marine Kelley <none@none>
2014-09-10 12:01:51 +0200

    Fix : @camavdist does not show silhouettes after a TP

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -3138,7 +3138,7 @@ bool LLVOAvatar::isVisuallyMuted()
 	if (!isSelf())
 	{
 //MK
-		if (gRRenabled && gAgentAvatarp)
+		if (gRRenabled && gAgentAvatarp && getRezzedStatus() >= 3) // fully rezzed & textured
 		{
 			LLVector3d my_head_pos (gAgent.getPosGlobalFromAgent(gAgentAvatarp->mHeadp->getWorldPosition()));
 			LLVector3d their_head_pos (gAgent.getPosGlobalFromAgent(mHeadp->getWorldPosition()));
