abbd22419fac2a2e8ead59db35ede69ce28a2e42
Nicky Perian <nickyperian@yahoo.com>
2014-10-07 21:07:56 -0500

    This reverses the new SL viewer login. LL stated the purpose is to have uses stick after first use. Kokua users usually are not new to virtual worlds. And, with multiple grids it is best to stick with a known login presentation. Will review the changesets of the new login panel and determine if any add value, if so, those will be cherry-picked.

--- indra/newview/llfloaterpreference.cpp
+++ indra/newview/llfloaterpreference.cpp
@@ -756,16 +756,6 @@ void LLFloaterPreference::initDoNotDisturbResponse()
 		}
 	}
 
-//static 
-void LLFloaterPreference::updateShowFavoritesCheckbox(bool val)
-{
-	LLFloaterPreference* instance = LLFloaterReg::findTypedInstance<LLFloaterPreference>("preferences");
-	if (instance)
-	{
-		instance->getChild<LLUICtrl>("favorites_on_login_check")->setValue(val);
-	}	
-}
-
 void LLFloaterPreference::setHardwareDefaults()
 {
 	LLFeatureManager::getInstance()->applyRecommendedSettings();
@@ -1619,7 +1609,6 @@ void LLFloaterPreference::setPersonalInfo(const std::string& visibility, bool im
 	getChildView("chat_font_size")->setEnabled(TRUE);
 }
 
-
 void LLFloaterPreference::refreshUI()
 {
 	refresh();
@@ -1912,8 +1901,6 @@ BOOL LLPanelPreference::postBuild()
 	if (hasChild("favorites_on_login_check", TRUE))
 	{
 		getChild<LLCheckBoxCtrl>("favorites_on_login_check")->setCommitCallback(boost::bind(&showFavoritesOnLoginWarning, _1, _2));
-		bool show_favorites_at_login = LLPanelLogin::getShowFavorites();
-		getChild<LLCheckBoxCtrl>("favorites_on_login_check")->setValue(show_favorites_at_login);
 	}
 
 	//////////////////////PanelAdvanced ///////////////////
--- indra/newview/llpanellogin.h
+++ indra/newview/llpanellogin.h
@@ -49,7 +49,13 @@ public:
 				void *callback_data);
 	~LLPanelLogin();
 
+	virtual BOOL handleKeyHere(KEY key, MASK mask);
+	virtual void draw();
 	virtual void setFocus( BOOL b );
+
+	// Show the XUI first name, last name, and password widgets.  They are
+	// hidden on startup for reg-in-client
+	static void showLoginWidgets();
 
 	static void show(const LLRect &rect,
 		void (*callback)(S32 option, void* user_data), 
@@ -61,7 +67,6 @@ public:
 
 	static BOOL areCredentialFieldsDirty();
 	static void setLocation(const LLSLURL& slurl);
-	static void autologinToLocation(const LLSLURL& slurl);
 	
 	/// Call when preferences that control visibility may have changed
 	static void updateLocationSelectorsVisibility();
@@ -69,8 +74,6 @@ public:
 	static void closePanel();
 
 	void setSiteIsAlive( bool alive );
-
-	void showLoginWidgets();
 
 	static void loadLoginPage();	
 	static void giveFocus();
@@ -83,21 +86,14 @@ public:
 	/// to be called from LLStartUp::setStartSLURL
 	static void onUpdateStartSLURL(const LLSLURL& new_start_slurl);
 
-	// called from prefs when initializing panel
-	static bool getShowFavorites();
-
 private:
 	friend class LLPanelLoginListener;
+	void reshapeBrowser();
 	void addFavoritesToStartLocation();
 	void addUsersWithFavoritesToUsername();
-	void onLocationEditChanged(LLUICtrl* ctrl);
-	void onSelectFavorite();
 	void onSelectServer();
 	void onLocationSLURL();
 
-	static void onClickConnectLast(void*);
-	static void onClickConnectFavorite(void*);
-	static void onClickConnectLocation(void*);
 	static void onClickConnect(void*);
 	static void onClickNewAccount(void*);
 	static void onClickVersion(void*);
@@ -107,23 +103,16 @@ private:
 	static void updateServerCombo();
 
 private:
+	LLPointer<LLUIImage> mLogoImage;
 	boost::scoped_ptr<LLPanelLoginListener> mListener;
-
-	void updateLoginButtons();
 
 	void			(*mCallback)(S32 option, void *userdata);
 	void*			mCallbackData;
 
 	BOOL            mPasswordModified;
-	bool			mShowFavorites;
 
 	static LLPanelLogin* sInstance;
 	static BOOL		sCapslockDidNotification;
-	bool			mFirstLoginThisInstall;
-	bool mFavoriteSelected;
-	unsigned int mUsernameLength;
-	unsigned int mPasswordLength;
-	unsigned int mLocationLength;
 };
 
 #endif
