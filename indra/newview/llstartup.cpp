6f311a1f2768c1698e9ac44da9310e783f274185
Nicky Perian <nickyperian@yahoo.com>
2015-01-24 06:33:41 +1000

    Ticket #327 Removing start-up RLV restrictions because the restrictions don't go away on Mac.

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -1671,6 +1671,7 @@ bool idle_startup()
 	//---------------------------------------------------------------------
 	if(STATE_WORLD_WAIT == LLStartUp::getStartupState())
 	{
+/*
 //MK
 		// We are beginning a session that may or may not have the avatar wear stuff
 		// that restricts from seeing the location, names or even to look around.
@@ -1678,11 +1679,13 @@ bool idle_startup()
 		// be flushed out by the garbage collector later, after the actual restrictions
 		// have been received.
 		// For this, we simulate the reception of those commands from a non-existent object.
+
 		if (gRRenabled)
 		{
 			gAgent.mRRInterface.handleCommand (LLUUID::generateNewID(), "camavdist:0=n,shownames=n,showloc=n,tploc=n,camdrawmin:1=n,camdrawmax:1.1=n,camdrawalphamin:0=n,camdrawalphamax:1=n,camtextures=n");
 		}
 //mk
+*/ 
 		LL_DEBUGS("AppInit") << "Waiting for simulator ack...." << LL_ENDL;
 		set_startup_status(0.59f, LLTrans::getString("LoginWaitingForRegionHandshake"), gAgent.mMOTD);
 		if(gGotUseCircuitCodeAck)
