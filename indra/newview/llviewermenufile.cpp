05bf52a96359f7424ef24d4c27eeebb0637d16ec
Nicky Perian <nickyperian@yahoo.com>
2015-04-09 14:28:59 -0500

    Ticket #340. Upload fee and UI control. Disabled all File Upload menu items when there is a 0$ balance. The Upload Permissions item remains enabled. On SL for Bulk upload there is an after the fact notification of insufficent balance. This notification isn't present on OpenSim and the uploads attempt to complete. Disabling the menu items prevent the attempted upload from happening.

--- indra/newview/llviewermenufile.cpp
+++ indra/newview/llviewermenufile.cpp
@@ -92,7 +92,8 @@ class LLFileEnableUploadModel : public view_listener_t
 {
 	bool handleEvent(const LLSD& userdata)
 	{
-		return true;
+		bool new_value = gStatusBar && LLGlobalEconomy::Singleton::getInstance() && (gStatusBar->getBalance() >= LLGlobalEconomy::Singleton::getInstance()->getPriceUpload());
+		return new_value;
 	}
 };
 
