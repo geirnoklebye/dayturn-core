3854c5039614880c80727c5d58c5a3f63afe898b
Nicky Perian <nickyperian@yahoo.com>
2015-04-27 00:10:25 -0500

    [Windows] Configuration complete.

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -2,18 +2,20 @@
 include(Prebuilt)
 if (USESYSTEMLIBS)
   set(USESYSTEMLIBS OFF)
-  if (LINUX)
+  use_prebuilt_binary(slvoice)
+  use_prebuilt_binary(libhunspell)
 #kokuafixme
 #   if(LINUX AND ${ARCH} STREQUAL "x86_64")
 #     use_prebuilt_binary(32bitcompatibilitylibs)
 #   endif(LINUX AND ${ARCH} STREQUAL "x86_64")
   set(USESYSTEMLIBS ON)
 else (USESYSTEMLIBS)
-    use_prebuilt_binary(libuuid)
-    use_prebuilt_binary(fontconfig)
-  endif (LINUX)
-  use_prebuilt_binary(libhunspell)
+
   use_prebuilt_binary(slvoice)
+  use_prebuilt_binary(fontconfig)
+  if(LINUX)
+     if (${ARCH} STREQUAL "x86_64")
+      use_prebuilt_binary(32bitcompatibilitylibs)
       # for mesh, this is built with colladadom and
       # KOKUAFIXME: should be packaged with colladadom the next time packaging it
       #use_prebuilt_binary(minizip)
