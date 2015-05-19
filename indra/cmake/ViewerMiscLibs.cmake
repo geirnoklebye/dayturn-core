369d3bd849bdf3422a0e0db0bf4001e0518e5473
Nicky Perian <nickyperian@yahoo.com>
2015-05-19 09:23:10 -0500

    [Linux 64 bit] WIP new tools libraries. <move 32bitcompatibilitylibs out of the way as the new autobuild doesn't allow file overwrites amoungst archives> not concerned about slvoice at this time.

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -14,7 +14,8 @@ else (USESYSTEMLIBS)
      use_prebuilt_binary(libuuid)
      use_prebuilt_binary(fontconfig)
      if (${ARCH} STREQUAL "x86_64")
-      use_prebuilt_binary(32bitcompatibilitylibs)
+#      use_prebuilt_binary(32bitcompatibilitylibs)
+# putting out of the away for now autobuild metadata does not allow file overwrites amoungst archives
       # for mesh, this is built with colladadom and
       # KOKUAFIXME: should be packaged with colladadom the next time packaging it
       #use_prebuilt_binary(minizip)
