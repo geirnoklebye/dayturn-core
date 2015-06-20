31a3e64d0bce432973bceaaabe14d37b0b74ea61
Drakeo <none@none>
2015-06-20 15:39:33 -0500

    add USL build scripts add configure.log to hgignore

---
+++ BuildMe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NPROC=$(nproc)
+export AUTOBUILD_PLATFORM_OVERRIDE='linux64'
+
+autobuild  configure -c ReleaseOS -- -DLL_TESTS:BOOL=OFF -USESYSTEMLIBS:BOOL=ON -DWORD_SIZE=64  -DFMODEX:BOOL=ON  -DFMODEX_SDK_DIR=/opt/fmodapi44454linux/  -DOPENAL:BOOL=ON -DPACKAGE:BOOL=ON 2>&1 |tee configure.log
+
+cd build-linux-x86_64  
+
+make -j$NPROC
+
