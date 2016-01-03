3928872bbac1778884db492a3ac7f98123dfc22c
Drakeo <flats_fixed@flatsfixedbicycles.com>
2016-01-03 07:34:09 -0600

    latest stabele chromium cef

--- indra/llplugin/llpluginclassmedia.cpp
+++ indra/llplugin/llpluginclassmedia.cpp
@@ -1524,11 +1524,11 @@ void LLPluginClassMedia::initializeUrlHistory(const LLSD& url_history)
 	LL_DEBUGS("Plugin") << "Sending history" << LL_ENDL;
 }
 
-//FS
+//DKO
 //void LLPluginClassMedia::setFlipY( bool enabled )
 //{
 //	LLPluginMessage message(LLPLUGIN_MESSAGE_CLASS_MEDIA_BROWSER, "cef_flipy");
 //	message.setValueBoolean("enable", enabled);
 //	sendMessage(message);
 //}
-//FS
+//DKO
