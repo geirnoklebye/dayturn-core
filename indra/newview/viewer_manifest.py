34ece3d7bed67ad21e21936106a1645eaddd95cc
Nicky Perian <nickyperian@yahoo.com>
2016-01-05 21:47:54 -0600

    Ticket #389. Merge error. Return icon selection by channel type. Types are Test, Release, Project, and Beta. Example  -DVIEWER_CHANNEL="\"Kokua Project NORLV"" Will put this in the archive or installer file name and present a "PROJ" named icon.

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -1118,9 +1118,9 @@ class LinuxManifest(ViewerManifest):
         icon_path = self.icon_path()
         print "DEBUG: icon_path '%s'" % icon_path
         if self.prefix(src=icon_path, dst="") :
-            self.path("kokua_icon.png")
+            self.path("kokua_icon.png","kokua_icon.png" )
             if self.prefix(src="",dst="res-sdl") :
-                self.path("kokua_icon.BMP")
+                self.path("kokua_icon.bmp","kokua_icon.BMP")
                 self.end_prefix("res-sdl")
             self.end_prefix(icon_path)
 
