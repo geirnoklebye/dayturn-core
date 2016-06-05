d6a7a07bbb220fea6800c14e6e324e633e2a0c31
Nicky Perian <nickyperian@yahoo.com>
2016-06-04 19:01:44 -0500

    Backed out changeset: 23bbd1697ac2 Put code back in that we thought was unused. And set XUI as per Ansariel Code Review. Head in a very dark place yet again.

--- indra/newview/llgrouplist.h
+++ indra/newview/llgrouplist.h
@@ -75,7 +75,9 @@ public:
 private:
 	void setDirty(bool val = true)		{ mDirty = val; }
 	void refresh();
-	void addNewItem(const LLUUID& id, const std::string& name, const LLUUID& icon_id, EAddPosition pos = ADD_BOTTOM);
+	// <FS:Ansariel> Mark groups hidden in profile
+	//void addNewItem(const LLUUID& id, const std::string& name, const LLUUID& icon_id, EAddPosition pos = ADD_BOTTOM);
+	void addNewItem(const LLUUID& id, const std::string& name, const LLUUID& icon_id, EAddPosition pos = ADD_BOTTOM, bool hiddenInProfile = false);
 	bool handleEvent(LLPointer<LLOldEvents::LLEvent> event, const LLSD& userdata); // called on agent group list changes
 
 	bool onContextMenuItemClick(const LLSD& userdata);
@@ -118,6 +120,8 @@ public:
 
 	virtual void changed(LLGroupChange gc);
 
+	// <FS:Ansariel> Mark groups hidden in profile
+	void markHiddenInProfile();
 private:
 	void setActive(bool active);
 	void onInfoBtnClick();
