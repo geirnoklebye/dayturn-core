ebf151094ab9b991613a9fbcd205e9f17d6e99fb
Nicky Perian <nickyperian@yahoo.com>
2016-09-14 08:30:58 -0500

    Apparent merge error. Sync to viewer-vlc

--- indra/media_plugins/cef/media_plugin_cef.cpp
+++ indra/media_plugins/cef/media_plugin_cef.cpp
@@ -139,10 +139,6 @@ MediaPluginBase(host_send_func, host_user_data)
 	mPopupH = 0;
 	mPopupX = 0;
 	mPopupY = 0;
-	mPopupX = 0;
-	mPopupY = 0;
-	// </FS:ND>
-
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -686,8 +682,7 @@ void MediaPluginCEF::receiveMessage(const char* message_string)
                 keyEvent(key_event, key, LLCEFLib::KM_MODIFIER_NONE, native_key_data);
 
 #endif
-//#elif LL_WINDOWS // <FS:ND/> Windows & Linux
-#else
+#else //Windows and Linux
 				std::string event = message_in.getValue("event");
 				S32 key = message_in.getValueS32("key");
 				std::string modifiers = message_in.getValue("modifiers");
