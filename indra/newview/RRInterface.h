16d5cb3968824e164350b08067077cf0e05e9b7f
Marine Kelley <none@none>
2016-11-28 20:22:07 +0100

    RLV 2.9.20.3

--- indra/newview/RRInterface.h
+++ indra/newview/RRInterface.h
@@ -30,9 +30,9 @@
 
 #define RR_VIEWER_NAME "RestrainedLife"
 #define RR_VIEWER_NAME_NEW "RestrainedLove"
-#define RR_VERSION_NUM "2092002"
-#define RR_VERSION "2.09.20.02"
-#define RR_SLV_VERSION "4.1.1.36943"
+#define RR_VERSION_NUM "2092003"
+#define RR_VERSION "2.09.20.03"
+#define RR_SLV_VERSION "4.1.2.37151"
 
 #define RR_PREFIX "@"
 #define RR_SHARED_FOLDER "#RLV"
