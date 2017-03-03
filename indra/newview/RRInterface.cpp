567d5b13c9483f9817360aa9d6535701e9a92d2f
Nicky Perian <nickyperian@yahoo.com>
2017-03-03 11:04:11 -0600

    [Linux] correct warning as error again and again

--- indra/newview/RRInterface.cpp
+++ indra/newview/RRInterface.cpp
@@ -552,7 +552,7 @@ RRInterface::~RRInterface()
 
 std::string RRInterface::getVersion ()
 {
-	return RR_VIEWER_NAME" viewer v"RR_VERSION" ("RR_SLV_VERSION")"; // there is no '+' between the string and the macro
+	return RR_VIEWER_NAME " viewer v " RR_VERSION " (" RR_SLV_VERSION")"; // there is no '+' between the string and the macro
 //	return RR_VIEWER_NAME" viewer v"RR_VERSION" ("+LLViewerInfo::getVersion()+")";
 }
 
