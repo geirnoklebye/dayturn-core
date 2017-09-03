c2afa0455c19be3f9027189fbd3f52acef54fe2f
Nicky Perian <nickyperian@yahoo.com>
2017-09-02 21:12:02 -0500

    Corrected merge for the same code again.

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -4583,7 +4583,7 @@ void LLViewerWindow::saveImageNumbered(LLImageFormatted *image, bool force_picke
     //	i++;
     //}
     //while( -1 != err );  // search until the file is not found (i.e., stat() gives an error).
-
+/*
 // Check if there is enough free space to save snapshot
 #ifdef LL_WINDOWS
 	boost::filesystem::space_info b_space = boost::filesystem::space(utf8str_to_utf16str(sSnapshotDir));
@@ -4595,7 +4595,7 @@ void LLViewerWindow::saveImageNumbered(LLImageFormatted *image, bool force_picke
 		insufficient_memory = TRUE;
 		return FALSE;
 	}
-
+*/
     // Get a base file location if needed.
     if (force_picker || !isSnapshotLocSet())
     {
