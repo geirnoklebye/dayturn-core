21edbf418a51206287d480377a69ef4d988fe94f
Nicky Perian <nickyperian@yahoo.com>
2017-09-04 16:25:38 -0500

    see (23189eb73088) MAINT-7641: Warn user if s/he directly runs viewer without launcher. Kokua at this point is unable to use SL_Launcher.exe. Its use results in up 3.5 minutes while the viewer get to the login page.

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1118,7 +1118,8 @@ bool LLAppViewer::init()
 
 		}
 	}
-
+//Comment out for NOW since kokua is unable to use LL's update scheme 
+/*
 	char* PARENT = getenv("PARENT");
 	if (! (PARENT && std::string(PARENT) == "SL_Launcher"))
 	{
@@ -1131,7 +1132,7 @@ bool LLAppViewer::init()
 		// him/herself in the foot.
 		LLNotificationsUtil::add("RunLauncher");
 	}
-
+*/
 #if LL_WINDOWS
 	if (gGLManager.mGLVersion < LLFeatureManager::getInstance()->getExpectedGLVersion())
 	{
