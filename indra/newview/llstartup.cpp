1af0c42e2e9f7f6a27b06ff523b20610d7a1203d
Ansariel <none@none>
2017-12-16 19:34:03 +0100

    Add support for FMOD Studio, originally by Drake Arconis (Alchemy Viewer), with FS-specific additions by Drakeo

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -41,6 +41,10 @@
 #include "llviewermedia_streamingaudio.h"
 #include "llaudioengine.h"
 
+#ifdef LL_FMODSTUDIO
+# include "llaudioengine_fmodstudio.h"
+#endif
+
 #ifdef LL_FMODEX
 # include "llaudioengine_fmodex.h"
 #endif
@@ -843,7 +847,16 @@ bool idle_startup()
 			delete gAudiop;
 			gAudiop = NULL;
 
-#ifdef LL_FMODEX		
+#ifdef LL_FMODSTUDIO
+#if !LL_WINDOWS
+			if (NULL == getenv("LL_BAD_FMODSTUDIO_DRIVER"))
+#endif // !LL_WINDOWS
+			{
+				gAudiop = (LLAudioEngine *) new LLAudioEngine_FMODSTUDIO(gSavedSettings.getBOOL("FMODExProfilerEnable"), gSavedSettings.getU32("FMODResampleMethod"));
+			}
+#endif
+
+#ifdef LL_FMODEX
 #if !LL_WINDOWS
 			if (NULL == getenv("LL_BAD_FMODEX_DRIVER"))
 #endif // !LL_WINDOWS
@@ -864,7 +877,7 @@ bool idle_startup()
 			if (gAudiop)
 			{
 #if LL_WINDOWS
-				// FMOD Ex on Windows needs the window handle to stop playing audio
+				// FMOD Studio and FMOD Ex on Windows needs the window handle to stop playing audio
 				// when window is minimized. JC
 				void* window_handle = (HWND)gViewerWindow->getPlatformWindow();
 #else
