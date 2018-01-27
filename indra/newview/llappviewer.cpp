1e260f080e6f22656b9ddfd80ecfe0adb21b1a90
Nicky Perian <nickyperian@yahoo.com>
2018-01-27 08:31:28 -0600

    [Windows] Based on user comments.  Use Kokua short cut and bypass SL_Launcher check

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1109,7 +1109,8 @@ bool LLAppViewer::init()
 
 		}
 	}
-
+//Comment out again based on user comments.
+/*
 	char* PARENT = getenv("PARENT");
 	if (! (PARENT && std::string(PARENT) == "SL_Launcher"))
 	{
@@ -1122,6 +1123,7 @@ bool LLAppViewer::init()
 		// him/herself in the foot.
 		LLNotificationsUtil::add("RunLauncher");
 	}
+ */
 
 #if LL_WINDOWS
 	if (gGLManager.mGLVersion < LLFeatureManager::getInstance()->getExpectedGLVersion())
