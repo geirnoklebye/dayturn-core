51cb58cca77064dea1a4ff5b1f0bc4d541219e97
Nicky Perian <nickyperian@yahoo.com>
2018-04-19 16:03:47 -0500

    Manual apply merge (dc70c7174694) DRTVWR-418: pull in new viewer-release  via viewer64

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -189,10 +189,9 @@ if (LINUX)
   # don't catch SIGCHLD in our base application class for the viewer - some of our 3rd party libs may need their *own* SIGCHLD handler to work.  Sigh!  The viewer doesn't need to catch SIGCHLD anyway.
   add_definitions(-DLL_IGNORE_SIGCHLD)
   if (ADDRESS_SIZE EQUAL 32)
-      add_definitions(-march=x86-64 -mfpmath=sse)
+    add_definitions(-march=pentium4)
   endif (ADDRESS_SIZE EQUAL 32)
-       add_definitions(-march=pentium4 -mfpmath=sse)
-    ENDIF(${ARCH} STREQUAL "x86_64")
+  add_definitions(-mfpmath=sse)
   #add_definitions(-ftree-vectorize) # THIS CRASHES GCC 3.1-3.2
   if (NOT USESYSTEMLIBS)
     # this stops us requiring a really recent glibc at runtime
