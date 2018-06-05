d19267e96038b9960d2650e085cd883d3f117723
Marine Kelley <none@none>
2018-06-05 12:11:59 +0200

    Remove the ability to add RLV commands to the blacklist (but it's still there and still answers to @getversionnumbl and @getblacklist messages, only the blacklist remains empty)

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -404,7 +404,12 @@ bool idle_startup()
 	RRInterface::sCanOoc = gSavedSettings.getBOOL("RestrainedLoveCanOoc");
 	RRInterface::sRecvimMessage = gSavedSettings.getString("RestrainedLoveRecvimMessage");
 	RRInterface::sSendimMessage = gSavedSettings.getString("RestrainedLoveSendimMessage");
+
+	// MK: Deactivated for now so we can't use the blacklist to remove capabilities.
+	// CA: Instead we use a new debug setting to manage this
 	RRInterface::sBlacklist = gSavedSettings.getString("RestrainedLoveBlacklist");
+	if (gSavedSettings.getBOOL("KokuaRLVDisableBlacklist")) RRInterface::sBlacklist = "";
+	
 	// Let's keep it constant for now, because there are ways to make the vision restriction less tight by playing with this setting.
 	//RRInterface::mCamDistNbGradients = gSavedSettings.getU32("RestrainedLoveCamDistNbGradients");
 	if (RRInterface::mCamDistNbGradients == 0)
