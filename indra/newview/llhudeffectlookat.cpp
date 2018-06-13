73dd840d8f643553d55f13c50b26ee508662b35a
Chorazin Allen <chorazinallen@gmail.com>
2018-06-13 18:55:13 +0100

    Manually apply fixes for highlighting of mesh objects causing issues with rendering of HUD-layer items (BUG-216387). Original fix by Ansariel Hiller.

--- indra/newview/llhudeffectlookat.cpp
+++ indra/newview/llhudeffectlookat.cpp
@@ -554,6 +554,7 @@ void LLHUDEffectLookAt::render()
 			)
 		)
 	)) {
+	    LLGLDisable gls_stencil(GL_STENCIL_TEST);
 		LLVector3 target = mTargetPos + ((LLVOAvatar*)(LLViewerObject*)mSourceObject)->mHeadp->getWorldPosition();
 		LLColor3 color = (*mAttentions)[mTargetType].mColor;
 
