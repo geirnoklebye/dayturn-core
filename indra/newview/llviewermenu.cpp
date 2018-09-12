19fccefd871691e187de23ba8fa18570bec24ed4
Marine Kelley <none@none>
2018-09-12 11:38:28 +0200

    Fix : Don't allow zooming in on another avatar when under @camunlock

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -6831,6 +6831,11 @@ void handle_look_at_selection(const LLSD& param)
 
 void handle_zoom_to_object(LLUUID object_id)
 {
+	if (gRRenabled && gAgent.mRRInterface.contains("camunlock"))
+	{
+		return;
+	}
+
 	const F32 PADDING_FACTOR = 2.f;
 
 	LLViewerObject* object = gObjectList.findObject(object_id);
