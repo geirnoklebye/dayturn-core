16ec875e3ff9559c35025de9592dc27e2426ebb6
Chorazin Allen <chorazinallen@gmail.com>
2019-01-23 16:15:08 +0000

    Demote a spammy log message to _ONCE

--- indra/newview/llviewerobjectlist.cpp
+++ indra/newview/llviewerobjectlist.cpp
@@ -1113,7 +1113,8 @@ void LLViewerObjectList::fetchObjectCostsCoro(std::string url)
 
     if (diff.empty())
     {
-        LL_INFOS() << "No outstanding object IDs to request. Pending count: " << mPendingObjectCost.size() << LL_ENDL;
+    		// CA This gets spammy for no obvious reason, so demote it to _ONCE
+        LL_INFOS_ONCE() << "No outstanding object IDs to request. Pending count: " << mPendingObjectCost.size() << LL_ENDL;
         return;
     }
 
