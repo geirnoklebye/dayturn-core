4ee80234980bff6df9e5cc17ad00a889e4b9e3e7
Nicky Perian <billnickels1942@gmail.com>
2019-02-09 09:50:40 -0600

    [MacOS] Complete Window Titles ported from Firestorm, By Tonya, Cinders, and maybe others.

--- indra/llwindow/llwindowmacosx-objc.h
+++ indra/llwindow/llwindowmacosx-objc.h
@@ -97,6 +97,7 @@ void hideNSCursorTillMove(bool hide);
 void requestUserAttention();
 long showAlert(std::string title, std::string text, int type);
 void setResizeMode(bool oldresize, void* glview);
+void setTitleCocoa(NSWindowRef window, const std::string &title);	// <FS:CR> Set Window title
 
 NSWindowRef createNSWindow(int x, int y, int width, int height);
 
