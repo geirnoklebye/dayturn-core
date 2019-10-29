f0db82ccee60db1a85e7daa83e82c293382042e6
Chorazin Allen <chorazinallen@gmail.com>
2019-10-29 22:08:51 +0000

    KKA-614 Back out SL-11598 - not finding a package shouldn't be a fatal error on the build since we expect to have either fmodex or fmodstudio available

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -46,7 +46,7 @@ viewer_dir = os.path.dirname(__file__)
 # Put it FIRST because some of our build hosts have an ancient install of
 # indra.util.llmanifest under their system Python!
 sys.path.insert(0, os.path.join(viewer_dir, os.pardir, "lib", "python"))
-from indra.util.llmanifest import LLManifest, main, path_ancestors, CHANNEL_VENDOR_BASE, RELEASE_CHANNEL, ManifestError, MissingError
+from indra.util.llmanifest import LLManifest, main, path_ancestors, CHANNEL_VENDOR_BASE, RELEASE_CHANNEL, ManifestError
 from llbase import llsd
 
 class ViewerManifest(LLManifest):
@@ -1020,15 +1020,11 @@ class DarwinManifest(ViewerManifest):
                     # (source, dest) pair to self.file_list for every expanded
                     # file processed. Remember its size before the call.
                     oldlen = len(self.file_list)
-                    try:
-                        self.path(src, dst)
-                        # The dest appended to self.file_list has been prepended
-                        # with self.get_dst_prefix(). Strip it off again.
-                        added = [os.path.relpath(d, self.get_dst_prefix())
-                                 for s, d in self.file_list[oldlen:]]
-                    except MissingError as err:
-                        print >> sys.stderr, "Warning: "+err.msg
-                        added = []
+                    self.path(src, dst)
+                    # The dest appended to self.file_list has been prepended
+                    # with self.get_dst_prefix(). Strip it off again.
+                    added = [os.path.relpath(d, self.get_dst_prefix())
+                             for s, d in self.file_list[oldlen:]]
                     if not added:
                         print "Skipping %s" % dst
                     return added
@@ -1884,9 +1880,4 @@ if __name__ == "__main__":
         dict(name='bugsplat', description="""BugSplat database to which to post crashes,
              if BugSplat crash reporting is desired""", default=''),
         ]
-    try:
-        main(extra=extra_arguments)
-    except (ManifestError, MissingError) as err:
-        sys.exit("\nviewer_manifest.py failed: "+err.msg)
-    except:
-        raise
+    main(extra=extra_arguments)
