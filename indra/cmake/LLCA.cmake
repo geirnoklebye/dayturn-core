1e1debc918acba46dff29415c1eb6267e4ce7e6c
Oz Linden <oz@lindenlab.com>
2020-06-04 12:25:19 -0400

    SL-13365 (debugging) explicitly use "bash" to invoke check script

--- indra/cmake/LLCA.cmake
+++ indra/cmake/LLCA.cmake
@@ -6,7 +6,7 @@ use_prebuilt_binary(llca)
 set(CA_BUNDLE_MAX_AGE_DAYS 90)
 
 execute_process(
-    COMMAND bin/check-ca-bundle-age.sh --packages . --days ${CA_BUNDLE_MAX_AGE_DAYS}
+    COMMAND bash bin/check-ca-bundle-age.sh --packages . --days ${CA_BUNDLE_MAX_AGE_DAYS}
     WORKING_DIRECTORY ${AUTOBUILD_INSTALL_DIR}
     RESULT_VARIABLE ca_bundle_check_result
     )
