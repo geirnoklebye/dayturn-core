f9804cd4fccb562809c32d0c3d0368ec4a1a581a
Chorazin Allen <chorazinallen@gmail.com>
2020-09-28 18:41:46 +0100

    KKA-761 Stop @shownames causing the script debug/warning floater to get confused

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -2710,6 +2710,14 @@ void process_chat_from_simulator(LLMessageSystem *msg, void **user_data)
 	msg->getU8("ChatData", "ChatType", type_temp);
 	chat.mChatType = (EChatType)type_temp;
 
+	//CA KKA-757 the script warning/debug floater needs a valid from_id to put stuff in the right panel
+	//   Although this is a bit clumsy (zap it, then unzap it) it'll make for easier merging
+	if (gRRenabled && chat.mChatType == CHAT_TYPE_DEBUG_MSG)
+	{
+		chat.mFromID = from_id;
+	}
+	//ca
+
 	msg->getU8Fast(_PREHASH_ChatData, _PREHASH_Audible, audible_temp);
 	chat.mAudible = (EChatAudible)audible_temp;
 
