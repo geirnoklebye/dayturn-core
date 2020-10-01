268646a6499c8c9bc287fb67ed931bc80e438f42
Chorazin Allen <chorazinallen@gmail.com>
2020-10-01 20:14:03 +0100

    Eliminate another dependency issue for Linux (with thanks to FS:Tonya Souther)

--- indra/cmake/LLPlugin.cmake
+++ indra/cmake/LLPlugin.cmake
@@ -8,7 +8,12 @@ set(LLPLUGIN_INCLUDE_DIRS
 if (LINUX)
     # In order to support using ld.gold on linux, we need to explicitely
     # specify all libraries that llplugin uses.
-    set(LLPLUGIN_LIBRARIES llplugin pthread)
+    # set(LLPLUGIN_LIBRARIES llplugin pthread)
+    # Version below from Firestorm, with thanks to Tonya Souther
+    set(CMAKE_THREAD_PREFER_PHREAD TRUE)
+    set(THREADS_PREFER_PHREAD_FLAG TRUE)
+    find_package(Threads QUIET REQUIRED)
+    set(LLPLUGIN_LIBRARIES llplugin Threads::Threads)
 else (LINUX)
     set(LLPLUGIN_LIBRARIES llplugin)
 endif (LINUX)
