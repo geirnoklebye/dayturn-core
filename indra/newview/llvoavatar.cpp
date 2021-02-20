0f0e85b3bf4cc4552ba3f2cb66b83a8028f7faa1
Chorazin Allen <chorazinallen@gmail.com>
2021-02-20 23:04:55 +0000

    KKA-812 Get the name tag distance colouring working

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -3505,7 +3505,6 @@ void LLVOAvatar::idleUpdateNameTagText(BOOL new_name)
 		|| complexity != mNameArc
 		|| complexity_color != mNameArcColor)
 	{
-		LLColor4 name_tag_color = getNameTagColor();
 
 		clearNameTag();
 
@@ -3668,6 +3667,7 @@ void LLVOAvatar::idleUpdateNameTagText(BOOL new_name)
 		mNameAppearance = is_appearance;
 		mNameFriend = is_friend;
 		mNameCloud = is_cloud;
+		mNameColor=name_tag_color;
 		mDistanceString = distance_string;
 		mTitle = title ? title->getString() : "";
 				// <FS:Ansariel> Show Arc in nametag (for Jelly Dolls)
