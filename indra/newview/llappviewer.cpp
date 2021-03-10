34a3dce1ee8c32a39d6b6a8b8b04e5c054643ba3
NickyP <nickyperian@gmail.com>
2021-03-10 17:55:21 -0600

    KKA-822 Implement Firestorm Toggle Crouch option Credit <FS:Ansariel> Chalice Yao's crouch toggle

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1648,6 +1648,14 @@ bool LLAppViewer::doFrame()
 			{
 				joystick->scanJoystick();
 				gKeyboard->scanKeyboard();
+				// <FS:Ansariel> Chalice Yao's crouch toggle
+				static LLCachedControl<bool> fsCrouchToggle(gSavedPerAccountSettings, "FSCrouchToggle");
+				static LLCachedControl<bool> fsCrouchToggleStatus(gSavedPerAccountSettings, "FSCrouchToggleStatus");
+				if (fsCrouchToggle && fsCrouchToggleStatus)
+				{
+					gAgent.moveUp(-1);
+			}
+				// </FS:Ansariel>
 			}
 
 //MK
