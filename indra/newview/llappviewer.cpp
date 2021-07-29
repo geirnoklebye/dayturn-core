129ce7edd8c2e86a2023851c0ff10ff221421377
Chorazin Allen <chorazinallen@gmail.com>
2021-07-29 11:37:34 +0100

    KKA-833 Port over Ansariel's code cleanup for Contact Sets to eliminate llmessage/llavatarnamecache pulling in newview and llxml headers

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -5880,6 +5880,7 @@ void LLAppViewer::disconnectViewer()
 			gInventory.getLibraryOwnerID());
 	}
 
+	LLAvatarNameCache::instance().setCustomNameCheckCallback(LLAvatarNameCache::custom_name_check_callback_t()); // <FS:Ansariel> Contact sets
 	saveNameCache();
 	if (LLExperienceCache::instanceExists())
 	{
