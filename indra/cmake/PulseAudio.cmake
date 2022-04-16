6364f3a5624b4c07a5bb71567f76cff81d02589e
Nicky <nicky.dasmijn@gmail.com>
2022-04-16 19:51:24 +0200

    Make Pulseaudio.cmake just error out. a) it is broken (there is no 3p for example), the define it sets it used. The vrabiables it did set are unused.
    There is a case to probe for pulseaudio and this is the Linux volume
    catcher. But for that it is only necessary to detect the system include
    to get a few defines and functions

--- indra/cmake/PulseAudio.cmake
+++ indra/cmake/PulseAudio.cmake
@@ -1,28 +1,4 @@
 # -*- cmake -*-
 include(Prebuilt)
 
-set(PULSEAUDIO ON CACHE BOOL "Build with PulseAudio support, if available.")
-
-if (PULSEAUDIO)
-  if (USESYSTEMLIBS)
-    include(FindPkgConfig)
-
-    pkg_check_modules(PULSEAUDIO libpulse)
-
-  elseif (LINUX)    
-    set(PULSEAUDIO_FOUND ON FORCE BOOL)
-    set(PULSEAUDIO_INCLUDE_DIRS
-    ${LIBS_PREBUILT_DIR}/include/glib
-        )
-    # We don't need to explicitly link against pulseaudio itself, because
-    # the viewer probes for the system's copy at runtime.
-    set(PULSEAUDIO_LIB_NAMES
-        glib
-        gobject
-      )
-  endif (USESYSTEMLIBS)
-endif (PULSEAUDIO)
-
-if (PULSEAUDIO_FOUND)
-  add_definitions(-DLL_PULSEAUDIO_ENABLED=1)
-endif (PULSEAUDIO_FOUND)
+message( FATAL_ERROR "Pulseaudio cmake file is broken" )
