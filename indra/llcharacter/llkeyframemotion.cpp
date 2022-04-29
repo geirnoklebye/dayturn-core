9efbbbcda1fdeb6d1a2f230885e21eb6f2c75d64
Mnikolenko Productengine <mnikolenko@productengine.com>
2022-04-29 19:06:22 +0300

    SL-17206 FIXED Unable to move an avatar after fall on the ground

--- indra/llcharacter/llkeyframemotion.cpp
+++ indra/llcharacter/llkeyframemotion.cpp
@@ -1347,6 +1347,14 @@ bool LLKeyframeMotion::deserialize(LLDataPacker& dp, const LLUUID& asset_id, boo
 		return false;
 	}
 
+	//SL-17206 hack to alter Female_land loop setting, while current behavior won't be changed serverside
+	LLUUID const female_land_anim("ca1baf4d-0a18-5a1f-0330-e4bd1e71f09e");
+	if (female_land_anim == asset_id)
+	{
+		LL_WARNS() << "Animation(" << female_land_anim << ") won't be looped." << LL_ENDL;
+		mJointMotionList->mLoop = FALSE;
+	}
+
 	//-------------------------------------------------------------------------
 	// get easeIn and easeOut
 	//-------------------------------------------------------------------------
