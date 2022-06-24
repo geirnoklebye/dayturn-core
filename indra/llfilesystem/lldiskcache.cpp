127e427db2cd4c93c72137ee99c78ad029a52c4c
Cosmic Linden <cosmic@lindenlab.com>
2022-06-24 13:32:14 -0700

    SL-17329: Remove unused extra variable initialization

--- indra/llfilesystem/lldiskcache.cpp
+++ indra/llfilesystem/lldiskcache.cpp
@@ -147,7 +147,6 @@ void LLDiskCache::purge()
         {
             file_removed.push_back(should_remove);
         }
-        std::string action = "";
         if (should_remove)
         {
             boost::filesystem::remove(entry.second.second, ec);
