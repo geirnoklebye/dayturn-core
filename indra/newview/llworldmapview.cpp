e19208c7fbc84a7c0383ad1eab5955f1e7d625cf
Mnikolenko Productengine <mnikolenko@productengine.com>
2022-07-18 16:53:43 +0300

    SL-17785 FIXED World Map scrolls too slowly when searching for a region.
    # Conflicts:
    #	indra/newview/llfloaterworldmap.cpp
    #	indra/newview/llworldmapview.cpp
    #	indra/newview/llworldmapview.h

--- indra/newview/llworldmapview.cpp
+++ indra/newview/llworldmapview.cpp
@@ -188,7 +188,8 @@ LLWorldMapView::LLWorldMapView()
 	mMouseDownY( 0 ),
     mSelectIDStart(0),
     mMapScale(0.f),
-    mTargetMapScale(0.f)
+    mTargetMapScale(0.f),
+    mMapIterpTime(MAP_ITERP_TIME_CONSTANT)
 {
     // LL_INFOS("WorldMap") << "Creating the Map -> LLWorldMapView::LLWorldMapView()" << LL_ENDL;
 
@@ -286,7 +287,7 @@ void LLWorldMapView::setScale(F32 scale, bool snap)
         {
             mMapScale = 0.1f;
         }
-
+        mMapIterpTime = MAP_ITERP_TIME_CONSTANT;
         F32 ratio = (scale / old_scale);
         mPanX *= ratio;
         mPanY *= ratio;
@@ -328,6 +329,7 @@ void LLWorldMapView::translatePan(S32 delta_x, S32 delta_y)
 // static
 void LLWorldMapView::setPan( S32 x, S32 y, bool snap )
 {
+    mMapIterpTime = MAP_ITERP_TIME_CONSTANT;
     mTargetPanX = (F32) x;
     mTargetPanY = (F32) y;
     if (snap)
@@ -338,7 +340,14 @@ void LLWorldMapView::setPan( S32 x, S32 y, bool snap )
     sVisibleTilesLoaded = false;
 }
 
-bool LLWorldMapView::showRegionInfo() { return LLWorldMipmap::scaleToLevel(mMapScale) <= DRAW_SIMINFO_THRESHOLD; }
+// static
+void LLWorldMapView::setPanWithInterpTime(S32 x, S32 y, bool snap, F32 interp_time)
+{
+    setPan(x, y, snap);
+    mMapIterpTime = interp_time;
+}
+
+bool LLWorldMapView::showRegionInfo() { return (LLWorldMipmap::scaleToLevel(mMapScale) <= DRAW_SIMINFO_THRESHOLD ? true : false); }
 
 ///////////////////////////////////////////////////////////////////////////////////
 // HELPERS
@@ -371,9 +380,9 @@ void LLWorldMapView::draw()
 
 	mVisibleRegions.clear();
 
-	// animate pan if necessary
-    mPanX = lerp(mPanX, mTargetPanX, LLSmoothInterpolation::getInterpolant(MAP_ITERP_TIME_CONSTANT));
-	mPanY = lerp(mPanY, mTargetPanY, LLSmoothInterpolation::getInterpolant(MAP_ITERP_TIME_CONSTANT));
+    // animate pan if necessary
+    mPanX = lerp(mPanX, mTargetPanX, LLSmoothInterpolation::getInterpolant(mMapIterpTime));
+    mPanY = lerp(mPanY, mTargetPanY, LLSmoothInterpolation::getInterpolant(mMapIterpTime));
     
     //RN: snaps to zoom value because interpolation caused jitter in the text rendering
     if (!sZoomTimer.getStarted() && mMapScale != mTargetMapScale)
