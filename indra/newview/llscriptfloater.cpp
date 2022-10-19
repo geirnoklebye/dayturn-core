79f43abe1cf472089c078f70c2f2a147b545defc
Rye Mutt <rye@alchemyviewer.org>
2022-10-19 19:42:39 -0400

    Fix use after free of LLUUID in script notifications

--- indra/newview/llscriptfloater.cpp
+++ indra/newview/llscriptfloater.cpp
@@ -465,10 +465,11 @@ void LLScriptFloaterManager::onAddNotification(const LLUUID& notification_id)
 
 		if(it != mNotifications.end())
 		{
+			LLUUID old_id = it->first; // copy LLUUID to prevent use after free when it is erased below
 			LLChicletPanel * chiclet_panelp = LLChicletBar::getInstance()->getChicletPanel();
 			if (NULL != chiclet_panelp)
 			{
-				LLIMChiclet * chicletp = chiclet_panelp->findChiclet<LLIMChiclet>(it->first);
+				LLIMChiclet * chicletp = chiclet_panelp->findChiclet<LLIMChiclet>(old_id);
 				if (NULL != chicletp)
 				{
 					// Pass the new_message icon state further.
@@ -477,14 +478,14 @@ void LLScriptFloaterManager::onAddNotification(const LLUUID& notification_id)
 				}
 			}
 
-			LLScriptFloater* floater = LLFloaterReg::findTypedInstance<LLScriptFloater>("script_floater", it->first);
+			LLScriptFloater* floater = LLFloaterReg::findTypedInstance<LLScriptFloater>("script_floater", old_id);
 			if (floater)
 			{
 				// Generate chiclet with a "new message" indicator if a docked window was opened but not in focus. See EXT-3142.
 				set_new_message |= !floater->hasFocus();
 			}
 
-			removeNotification(it->first);
+			removeNotification(old_id);
 		}
 	}
 
