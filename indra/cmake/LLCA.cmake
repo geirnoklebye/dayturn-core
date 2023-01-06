1f7cbdec4dbce1d116558d6d90a4b4800fc8ef9c
Geir Nøklebye <geir.noklebye@dayturn.com>
2023-01-06 14:46:46 +0100

    Up CA_BUNDLE_MAX_AGE_DAYS to 465 days

--- indra/cmake/LLCA.cmake
+++ indra/cmake/LLCA.cmake
@@ -3,7 +3,7 @@ include(Prebuilt)
 
 use_prebuilt_binary(llca)
 
-set(CA_BUNDLE_MAX_AGE_DAYS 365)
+set(CA_BUNDLE_MAX_AGE_DAYS 465)
 
 execute_process(
     COMMAND bash bin/check-ca-bundle-age.sh --packages . --days ${CA_BUNDLE_MAX_AGE_DAYS}
