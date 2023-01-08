f5348869357313dbfcfdd0988d0fa6d3364803ca
Geir Nøklebye <geir.noklebye@dayturn.com>
2023-01-08 11:36:06 +0100

    Print a message if the llca package is outdated, and not stop the build.

--- indra/cmake/LLCA.cmake
+++ indra/cmake/LLCA.cmake
@@ -3,7 +3,7 @@ include(Prebuilt)
 
 use_prebuilt_binary(llca)
 
-set(CA_BUNDLE_MAX_AGE_DAYS 465)
+set(CA_BUNDLE_MAX_AGE_DAYS 365)
 
 execute_process(
     COMMAND bash bin/check-ca-bundle-age.sh --packages . --days ${CA_BUNDLE_MAX_AGE_DAYS}
@@ -12,7 +12,7 @@ execute_process(
     )
 
 if (NOT ${ca_bundle_check_result} EQUAL 0)
-  message(SEND_ERROR "Certificate Authority Bundle is out of date (rebuild and update the llca package)")
+  message(STATUS "Certificate Authority Bundle is out of date (rebuild and update the llca package)")
 else (NOT ${ca_bundle_check_result} EQUAL 0)
   message(STATUS "Certificate Authority Bundle is recent enough.")
 endif (NOT ${ca_bundle_check_result} EQUAL 0)
