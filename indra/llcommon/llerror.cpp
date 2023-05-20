0a6c724d62655c3e3173c3e08122ca8771566d5e
Geir Nøklebye <geir.noklebye@dayturn.com>
2023-05-20 12:31:48 +0200

    More sync to LL 6.6.12 release version in llcommon

--- indra/llcommon/llerror.cpp
+++ indra/llcommon/llerror.cpp
@@ -579,11 +579,9 @@ namespace
 	
 	void Globals::invalidateCallSites()
 	{
-		for (CallSiteVector::const_iterator i = callSites.begin();
-			 i != callSites.end();
-			 ++i)
+		for (LLError::CallSite* site : callSites)
 		{
-            (*i)->invalidate();
+            site->invalidate();
 		}
 		
 		callSites.clear();
@@ -1211,12 +1209,8 @@ namespace
         std::string escaped_message;
 
         LLMutexLock lock(&s->mRecorderMutex);
-		for (Recorders::const_iterator i = s->mRecorders.begin();
-			i != s->mRecorders.end();
-			++i)
+		for (LLError::RecorderPtr& r : s->mRecorders)
 		{
-			LLError::RecorderPtr r = *i;
-
             if (!r->enabled())
             {
                 continue;
@@ -1371,8 +1365,7 @@ namespace LLError
 
 		site.mCached = true;
 		g->addCallSite(site);
-		site.mShouldLog = site.mLevel >= compareLevel;
-		return site.mShouldLog;
+		return site.mShouldLog = site.mLevel >= compareLevel;
 	}
 
 
