9d0a8799ca6f7b78a3b02bed245ea5b6b39a2bd3
Mnikolenko Productengine <mnikolenko@productengine.com>
2023-10-23 16:08:46 +0300

    SL-13610 build fix

--- indra/newview/llviewerjoystick.cpp
+++ indra/newview/llviewerjoystick.cpp
@@ -1460,7 +1460,7 @@ void LLViewerJoystick::saveDeviceIdToSettings()
     // someone editing the xml will corrupt it
     // so convert to string first
     std::string device_string = getDeviceUUIDString();
-    gSavedSettings.setLLSD("JoystickDeviceUUID", LLSD(device_string);
+    gSavedSettings.setLLSD("JoystickDeviceUUID", LLSD(device_string));
 #else
     LLSD device_id = getDeviceUUID();
     gSavedSettings.setLLSD("JoystickDeviceUUID", device_id);
