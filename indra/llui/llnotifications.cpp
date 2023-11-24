deac1b8a5b515c8c1bcbb9666c5d1a822aa5c567
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-11-24 02:23:20 +0200

    SL-17076 Thread safety of LLNotificationChannelBase's items
    Due to odd crashes when cleaning mItems adding thread safety
    Viewer runs window in a separate thread, it is possible notification
    was added in a way that corrupted the list.

--- indra/llui/llnotifications.cpp
+++ indra/llui/llnotifications.cpp
@@ -993,6 +993,7 @@ LLBoundListener LLNotificationChannelBase::connectChangedImpl(const LLEventListe
 	// all of the notifications that are already in the channel
 	// we use a special signal called "load" in case the channel wants to care
 	// only about new notifications
+    LLMutexLock lock(&mItemsMutex);
 	for (LLNotificationSet::iterator it = mItems.begin(); it != mItems.end(); ++it)
 	{
 		slot(LLSD().with("sigtype", "load").with("id", (*it)->id()));
@@ -1185,17 +1186,7 @@ bool LLNotificationChannel::isEmpty() const
 
 S32 LLNotificationChannel::size() const
 {
-	return mItems.size();
-}
-
-LLNotificationChannel::Iterator LLNotificationChannel::begin()
-{
-	return mItems.begin();
-}
-
-LLNotificationChannel::Iterator LLNotificationChannel::end()
-{
-	return mItems.end();
+    return mItems.size();
 }
 
 size_t LLNotificationChannel::size()
@@ -1203,12 +1194,19 @@ size_t LLNotificationChannel::size()
 	return mItems.size();
 }
 
+void LLNotificationChannel::forEachNotification(NotificationProcess process)
+{
+    LLMutexLock lock(&mItemsMutex);
+    std::for_each(mItems.begin(), mItems.end(), process);
+}
+
 std::string LLNotificationChannel::summarize()
 {
 	std::string s("Channel '");
 	s += mName;
 	s += "'\n  ";
-	for (LLNotificationChannel::Iterator it = begin(); it != end(); ++it)
+    LLMutexLock lock(&mItemsMutex);
+	for (LLNotificationChannel::Iterator it = mItems.begin(); it != mItems.end(); ++it)
 	{
 		s += (*it)->summarize();
 		s += "\n  ";
@@ -1735,6 +1733,7 @@ void LLNotifications::cancel(LLNotificationPtr pNotif)
 
 void LLNotifications::cancelByName(const std::string& name)
 {
+    LLMutexLock lock(&mItemsMutex);
 	std::vector<LLNotificationPtr> notifs_to_cancel;
 	for (LLNotificationSet::iterator it=mItems.begin(), end_it = mItems.end();
 		it != end_it;
@@ -1759,6 +1758,7 @@ void LLNotifications::cancelByName(const std::string& name)
 
 void LLNotifications::cancelByOwner(const LLUUID ownerId)
 {
+    LLMutexLock lock(&mItemsMutex);
 	std::vector<LLNotificationPtr> notifs_to_cancel;
 	for (LLNotificationSet::iterator it = mItems.begin(), end_it = mItems.end();
 		 it != end_it;
@@ -1804,11 +1804,6 @@ LLNotificationPtr LLNotifications::find(LLUUID uuid)
 	{
 		return *it;
 	}
-}
-
-void LLNotifications::forEachNotification(NotificationProcess process)
-{
-	std::for_each(mItems.begin(), mItems.end(), process);
 }
 
 std::string LLNotifications::getGlobalString(const std::string& key) const
