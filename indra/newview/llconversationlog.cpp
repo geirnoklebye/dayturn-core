30f5dbc511eecf01b0091fea71948804286eb5ed
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-04-10 22:54:31 +0300

    triage#170 Fix 'Clear log' button having incorrect state
    1. onClear should clear file even if in 'don't log' state.
    2. When chat isn't logging, check presence of old log by checking file.

--- indra/newview/llconversationlog.cpp
+++ indra/newview/llconversationlog.cpp
@@ -646,7 +646,7 @@ void LLConversationLog::onClearLogResponse(const LLSD& notification, const LLSD&
 	{
 		mConversations.clear();
 		notifyObservers();
-		cache();
+        saveToFile(getFileName());
 		deleteBackupLogs();
 	}
 }
