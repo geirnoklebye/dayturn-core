9f9dc40dbc055851f82b141c912e06a983cd9004
Rye Mutt <rye@alchemyviewer.org>
2024-07-01 22:25:56 -0400

    Use heterogeneous comparison for string_view map finds in LLControl and convert controlExists to string_view

--- indra/llui/lluictrl.cpp
+++ indra/llui/lluictrl.cpp
@@ -135,7 +135,7 @@ void LLUICtrl::initFromParams(const Params& p)
 	{
 		if (p.enabled_controls.enabled.isChosen())
 		{
-			LLControlVariable* control = findControl(p.enabled_controls.enabled);
+			LLControlVariable* control = findControl(p.enabled_controls.enabled());
 			if (control)
 			{
 				setEnabledControlVariable(control);
@@ -149,7 +149,7 @@ void LLUICtrl::initFromParams(const Params& p)
 		}
 		else if(p.enabled_controls.disabled.isChosen())
 		{
-			LLControlVariable* control = findControl(p.enabled_controls.disabled);
+			LLControlVariable* control = findControl(p.enabled_controls.disabled());
 			if (control)
 			{
 				setDisabledControlVariable(control);
@@ -169,7 +169,7 @@ void LLUICtrl::initFromParams(const Params& p)
 		if (p.controls_visibility.visible.isProvided())
 		// </FS:Zi>
 		{
-			LLControlVariable* control = findControl(p.controls_visibility.visible);
+			LLControlVariable* control = findControl(p.controls_visibility.visible());
 			if (control)
 			{
 				setMakeVisibleControlVariable(control);
@@ -186,7 +186,7 @@ void LLUICtrl::initFromParams(const Params& p)
 		if (p.controls_visibility.invisible.isProvided())
 		// </FS:Zi>
 		{
-			LLControlVariable* control = findControl(p.controls_visibility.invisible);
+			LLControlVariable* control = findControl(p.controls_visibility.invisible());
 			if (control)
 			{
 				setMakeInvisibleControlVariable(control);
