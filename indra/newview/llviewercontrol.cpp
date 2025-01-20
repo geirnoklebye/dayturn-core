e551227e6280f5dcdd5a58f6a8b0c36bb03fd006
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2025-01-20 22:09:20 +0200

    #3436 Better handling in setting_get_control

--- indra/newview/llviewercontrol.cpp
+++ indra/newview/llviewercontrol.cpp
@@ -713,9 +713,9 @@ LLPointer<LLControlVariable> setting_get_control(LLControlGroup& group, const st
     LLPointer<LLControlVariable> cntrl_ptr = group.getControl(setting);
     if (cntrl_ptr.isNull())
     {
+        LLError::LLUserWarningMsg::showMissingFiles();
         LL_ERRS() << "Unable to set up setting listener for " << setting
-            << ". Please reinstall viewer from https://www.dayturn.com/viewer/index.php?resources/ and contact Dayturn support if issue persists after reinstall."
-            << LL_ENDL;
+            << "." << LL_ENDL;
     }
     return cntrl_ptr;
 }
