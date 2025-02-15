d6e66dbe81d38687058a51c0f8f18e1d884df129
Geir Nøklebye <geir.noklebye@dayturn.com>
2025-02-15 09:41:40 +0100

    DAYTW-40 Adjust UDP socket SEND_ and RECEIVE_BUFFER_SIZE for macOS

--- indra/llmessage/net.cpp
+++ indra/llmessage/net.cpp
@@ -81,8 +81,8 @@ const char* BROADCAST_ADDRESS_STRING = "255.255.255.255";
 	// System (default) values can be found by sysctl kern.ipc.maxsockbuf which is the combined value
 	// of the send and receive buffer. Numbers below is 256kB per buffer for a total of 
 	// 262144 * 2 * 8 = 4194304 bit. Default on most newer systems is 6291456, so the setting is conservative. 
-	const int	SEND_BUFFER_SIZE	= 262144;
-	const int	RECEIVE_BUFFER_SIZE	= 262144;
+	const int	SEND_BUFFER_SIZE	= 327680;
+	const int	RECEIVE_BUFFER_SIZE	= 327680;
 #else // LL_DARWIN
 	const int	SEND_BUFFER_SIZE	= 400000;
 	const int	RECEIVE_BUFFER_SIZE	= 400000;
