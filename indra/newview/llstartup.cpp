94504b183f1e12122059c413cf0ed1370b3126ef
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2025-03-13 00:12:58 -0400

    Quick fix for a friend status race condition on login.

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -1331,6 +1331,15 @@ bool idle_startup()
 		display_startup();
 		display_startup();
 
+
+        // It is entirely possible that we may get the friends list _before_ we have the callbacks registered to process that.
+        // This will lead to the friends list not being processed properly and online statuses not being updated appropriately at login.
+        // So, we need to make sure that we have the callbacks registered before we get the friends list.
+        // -Geenz 2025-03-12
+        LL_INFOS() << " AvatarTracker" << LL_ENDL;
+        LLAvatarTracker::instance().registerCallbacks(gMessageSystem);
+		display_startup();
+
 		// Since we connected, save off the settings so the user doesn't have to
 		// type the name/password again if we crash.
 		gSavedSettings.saveToFile(gSavedSettings.getString("ClientSettingsFile"), true);
@@ -1996,8 +2005,6 @@ bool idle_startup()
 		LLMessageSystem* msg = gMessageSystem;
 		LL_INFOS() << " Inventory" << LL_ENDL;
 		LLInventoryModel::registerCallbacks(msg);
-		LL_INFOS() << " AvatarTracker" << LL_ENDL;
-		LLAvatarTracker::instance().registerCallbacks(msg);
 		LL_INFOS() << " Landmark" << LL_ENDL;
 		LLLandmark::registerCallbacks(msg);
 		display_startup();
