99548b4642307c538caab3a154161f3a6dd80328
Maxim Nikolenko <maximnproductengine@lindenlab.com>
2025-03-27 18:02:19 +0200

    #3820 crash at LLNotificationTemplate

--- indra/llui/llnotifications.cpp
+++ indra/llui/llnotifications.cpp
@@ -1249,7 +1249,8 @@ LLNotifications::LLNotifications()
 
 void LLNotifications::clear()
 {
-   mDefaultChannels.clear();
+    mDefaultChannels.clear();
+    mTemplates.clear();
 }
 
 // The expiration channel gets all notifications that are cancelled
