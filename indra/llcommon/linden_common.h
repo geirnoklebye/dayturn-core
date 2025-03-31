2049d07a7d7cf8738a22d79f7a3d4d6fe17ce733
Geir Nøklebye <geir.noklebye@dayturn.com>
2025-03-31 13:36:20 +0200

    DAYTW-32 We can also remove llboool.h for linden_common.h

--- indra/llcommon/linden_common.h
+++ indra/llcommon/linden_common.h
@@ -64,7 +64,6 @@ void  tracy_aligned_free(void *memblock);
 // Linden only libs in alpha-order other than stdtypes.h
 // *NOTE: Please keep includes here to a minimum, see above.
 #include "stdtypes.h"
-#include "llbool.h"
 #include "lldefs.h"
 #include "llerror.h"
 #include "llfile.h"
