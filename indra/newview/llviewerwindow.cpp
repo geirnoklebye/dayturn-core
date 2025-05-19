2c7754248e5b420a11a4787e4bba78d43808c0c9
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2025-05-19 21:51:55 +0300

    #4101 Handle boost::filesystem's exceptions

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -5479,7 +5479,18 @@ void LLViewerWindow::saveImageLocal(LLImageFormatted *image, const snapshot_save
 #else
 	boost::filesystem::path b_path(lastSnapshotDir);
 #endif
-	if (!boost::filesystem::is_directory(b_path))
+    boost::system::error_code ec;
+    if (!boost::filesystem::is_directory(b_path, ec) || ec.failed())
+    {
+        LLSD args;
+        args["PATH"] = lastSnapshotDir;
+        LLNotificationsUtil::add("SnapshotToLocalDirNotExist", args);
+        resetSnapshotLoc();
+        failure_cb();
+        return;
+    }
+    boost::filesystem::space_info b_space = boost::filesystem::space(b_path, ec);
+    if (ec.failed())
 	{
 		LLSD args;
 		args["PATH"] = lastSnapshotDir;
@@ -5488,7 +5499,6 @@ void LLViewerWindow::saveImageLocal(LLImageFormatted *image, const snapshot_save
 		failure_cb();
 		return;
 	}
-	boost::filesystem::space_info b_space = boost::filesystem::space(b_path);
 	if (b_space.free < image->getDataSize())
 	{
 		LLSD args;
@@ -5505,6 +5515,8 @@ void LLViewerWindow::saveImageLocal(LLImageFormatted *image, const snapshot_save
 		LLNotificationsUtil::add("SnapshotToComputerFailed", args);
 
 		failure_cb();
+
+        // Shouldn't there be a return here?
 	}
 	
 	// Look for an unused file name
