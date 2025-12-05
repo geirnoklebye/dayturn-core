c5bc04557781dda5432fbe423714f0da91cf34e6
Andrey Kleshchev <117672381+akleshchev@users.noreply.github.com>
2025-12-05 16:29:04 +0200

    #3612 "Copy SLURL" from Favorites bar not working
    If region isn't online, can't get region position and form slurl

--- indra/newview/llfavoritesbar.cpp
+++ indra/newview/llfavoritesbar.cpp
@@ -1297,6 +1297,13 @@ bool LLFavoritesBarCtrl::enableSelected(const LLSD& userdata)
     {
         return !LLAgentPicksInfo::getInstance()->isPickLimitReached();
     }
+    else if (param == "copy_slurl"
+             || param == "show_on_map")
+    {
+        LLVector3d posGlobal;
+        LLLandmarkActions::getLandmarkGlobalPos(mSelectedItemID, posGlobal);
+        return !posGlobal.isExactlyZero();
+    }
 
     return false;
 }
