c451428626d58115f77fd4c17135c6e569a03a15
Geir Nøklebye <geir.noklebye@dayturn.com>
2026-04-20 19:29:42 +0200

    Add getHash() to llmaterial just for the fun of it, and future use

--- indra/llprimitive/llmaterial.h
+++ indra/llprimitive/llmaterial.h
@@ -125,6 +125,7 @@ public:
     bool        operator != (const LLMaterial& rhs) const;
 
     U32         getShaderMask(U32 alpha_mode, bool is_alpha);
+    LLUUID      getHash() const;
 
 protected:
     LLUUID      mNormalID;
