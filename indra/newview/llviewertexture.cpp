77a929b2d7006d1e0f5d2e3cbf276b0a30e89920
Geir Nøklebye <geir.noklebye@dayturn.com>
2026-07-08 18:58:13 +0200

    Move the comment block extent to satisfy VS's sensibilities

--- indra/newview/llviewertexture.cpp
+++ indra/newview/llviewertexture.cpp
@@ -4003,8 +4003,8 @@ void LLViewerMediaTexture::setPlaying(bool playing)
 		for(std::list< LLFace* >::iterator iter = mMediaFaceList.begin(); iter!= mMediaFaceList.end(); ++iter)
 		{
             LLFace* facep = *iter;
-            const LLTextureEntry* te = facep->getTextureEntry();
 /* PBR materials not implemented in this viewer #1943
+            const LLTextureEntry* te = facep->getTextureEntry();
             if (te->getGLTFMaterial())
             {
                 // PBR material, switch emissive and basecolor
